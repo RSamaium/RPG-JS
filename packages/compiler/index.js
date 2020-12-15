d341f17baa01b517392aa87bbca5413f9d32be6c
Samuel Ronce <sam@webcreative5.net>
2020-12-15 15:52:21 +0100

    webpack compiler package

---
+++ packages/compiler/index.js
@@ -0,0 +1,8 @@
+const configServer = require('./webpack-server')
+const configClient = require('./webpack-client')
+
+const side = process.env.RPG_SIDE
+
+module.exports = function(path, { extendClient, extendServer } = {}) {
+    return side == 'server' ? configServer(path, extendServer) : configClient(path, extendClient)
+}
---
+++ packages/compiler/webpack-server.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+const mode = process.env.NODE_ENV || 'development'
+
+module.exports = function(dirname, extend = {}) {
+   return {
+        target: 'node',
+        node: {
+            __dirname: false
+        },
+        externals: [nodeExternals({
+            //allowlist: ['@rpgjs/default-gui', '@rpgjs/starter-kit-server', '@rpgjs/starter-kit-client']
+            allowlist: [/^@rpgjs/, 'lance-gg']
+        })],
+        mode,
+        entry: `./src/server/main.ts`,
+        output: {
+            path: path.join(dirname, 'dist/server'),
+            filename: 'index.js'
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        onlyCompileBundledFiles: true
+                    }
+                }],
+                exclude: [/node_modules/]
+            }, 
+            {
+                test: /\.tmx$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath(url)  {
+                                return `maps/${url.replace(/.tmx$/, '.json')}`
+                            },
+                            esModule: false
+                        }
+                    },  {
+                        loader: 'tmx-loader'
+                    }]
+                }]
+        },
+        optimization: {
+            minimize: false
+        },
+        plugins: [
+            new CleanWebpackPlugin()
+        ],
+        ...extend
+    }
+}
