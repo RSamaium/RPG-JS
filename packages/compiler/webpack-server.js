6d09b58d6ca1c63605877e223f36300cf7b7f500
Samuel Ronce <sam@webcreative5.net>
2020-12-29 16:06:56 +0100

    ts loader

--- packages/compiler/webpack-server.js
+++ packages/compiler/webpack-server.js
@@ -33,8 +33,7 @@ module.exports = function(dirname, extend = {}) {
                         options: {
                             onlyCompileBundledFiles: true
                         }
-                    }],
-                    exclude: [/node_modules/]
+                    }]
                 },
                 ...webpackCommon(dirname)
             ]
