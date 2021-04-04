834b6ff118da2d5e1c16c33163c9bf04105012f0
Samuel Ronce <sam@webcreative5.net>
2021-04-04 17:02:00 +0200

    jest config

--- packages/sample/src/server/rpg.ts
+++ packages/sample/src/server/rpg.ts
@@ -2,6 +2,10 @@ import { RpgServer, RpgServerEngine } from '@rpgjs/server'
 import { maps, database } from '@rpgjs/starter-kit-server'
 import { Player } from './player'
 
+import RpgMonitoringPlugin from '@rpgjs/plugin-monitoring'
+
+console.log(RpgMonitoringPlugin)
+
 @RpgServer({
     basePath: __dirname,
     maps,
