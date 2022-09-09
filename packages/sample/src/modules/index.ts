0e27eb858168889f66d80938c3c11f56ee5842ff
Samuel Ronce <sam@webcreative5.net>
2022-09-09 09:23:04 +0200

    fix component

--- packages/sample/src/modules/index.ts
+++ packages/sample/src/modules/index.ts
@@ -3,6 +3,8 @@ import defaultGui from '@rpgjs/default-gui'
 import mobileGui from '@rpgjs/mobile-gui'
 import gamepad from '@rpgjs/gamepad'
 import emotionBubblesPlugin from '@rpgjs/plugin-emotion-bubbles'
+import title from '@rpgjs/title-screen'
+import chat from '@rpgjs/chat'
 
 export default [
     main,
@@ -11,6 +13,6 @@ export default [
     defaultGui,
     //mobileGui,
     emotionBubblesPlugin,
-    //chat,
-    //title
-]
+    chat,
+   // title
+] 
