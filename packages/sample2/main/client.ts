a220e6355f9fa2b855bfc18eff06e4e9f49e10d2
RSamaium <sam@newbot.io>
2023-08-21 11:50:43 +0200

    fix timeline

--- packages/sample2/main/client.ts
+++ packages/sample2/main/client.ts
@@ -1,4 +1,50 @@
-import { RpgClientEngine } from "@rpgjs/client"
+import { RpgClientEngine, Timeline, Ease } from "@rpgjs/client"
+
+const animations = new Timeline()
+.add(40, ({ scale }) => [{
+    frameX: 0,
+    frameY: 1,
+    scale: [scale],
+}], {
+    scale: {
+        from: 1,
+        to: 1.3,
+        easing: Ease.easeInBounce
+    }
+})
+.add(40, ({ scale }) => [{
+    frameX: 1,
+    frameY: 1,
+    scale: [scale],
+}], {
+    scale: {
+        from: 1.3,
+        to: 1.5,
+    }
+})
+.add(40, ({ scale }) => [{
+    frameX: 3,
+    frameY: 1,
+    scale: [scale],
+}], {
+    scale: {
+        from: 1.5,
+        to: 1.3,
+    }
+})
+.add(40, ({ scale }) => [{
+    frameX: 2,
+    frameY: 1,
+    scale: [scale],
+}], {
+    scale: {
+        from: 1.3,
+        to: 1.2,
+    }
+})
+.create()
+
+console.log(animations)
 
 export default {
     async onStart(engine: RpgClientEngine) {
---
+++ tests/unit-tests/specs/timeline.spec.ts
@@ -0,0 +1,6 @@
+import { Ease } from '@rpgjs/client';
+import { describe, test } from 'vitest'
+
+describe('Test Ease Function', () => {
+
+})
