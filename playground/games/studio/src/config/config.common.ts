a280575baed87da7b0fc5062725a390faff4001e
RSamaium <sam@webcreative5.net>
2026-07-24 09:01:42 +0200

    chore: use Studio preproduction project

--- playground/games/studio/src/config/config.common.ts
+++ playground/games/studio/src/config/config.common.ts
@@ -7,10 +7,10 @@
 // }
 
 export const studio = {
-  projectId: "d8d6c955-3d7f-421d-9ffc-edf99a940fbc",
-  startMapId: "77a61765-58cd-41fa-a1a9-7f71ec2333cb",
-  apiUrl: "https://rpgjs.studio/api",
-  assetsUrl: "https://assets.rpgjs.studio",
+  projectId: "c058981c-3af2-4630-a7bb-37b5b72ab287",
+  startMapId: "b73228b2-9319-4328-9b32-fa09d9e220b4?",
+  apiUrl: "https://preprod.rpgjs.studio/api",
+  assetsUrl: "https://assets.preprod.rpgjs.studio",
   streaming: {
     chunkSize: 16,
     loadRadius: 2,
